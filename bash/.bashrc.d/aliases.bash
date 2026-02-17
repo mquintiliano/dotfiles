16f14fb5bb2b7e5a2cdf1ad6ea532a5bbcc334cc
Marcelo Quintiliano <2864057+mquintiliano@users.noreply.github.com>
2026-02-17 10:39:04 -0300

    chore: Consolidate aliases

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -1,7 +1,13 @@
 ### Aliases ###
 
-# neovim - Nvim
 alias vi='/usr/bin/nvim'
+alias upgrade='sudo dnf upgrade --refresh'
+alias vpn='nmcli con up São\ Paulo\ \(GRU\) --ask'
+alias fpr='flatpak run'
 
-# Run flatpak codium from host terminal
-alias codium="flatpak run com.vscodium.codium --no-sandbox "
+# tmux aliases
+alias tm='tmux new -s main'
+alias tml='tmux ls'
+
+# Validate YAML files
+alias yamlcheck='python -c "import sys, yaml as y; y.safe_load(open(sys.argv[1]))"'
