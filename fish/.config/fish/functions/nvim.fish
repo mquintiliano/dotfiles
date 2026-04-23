75d529b3de937946c45d7155ecfeb5ab63a911b2
Marcelo Quintiliano <2864057+mquintiliano@users.noreply.github.com>
2026-04-23 16:54:34 -0300

    chore: Update nvim.fish function[alias]

---
+++ fish/.config/fish/functions/nvim.fish
@@ -0,0 +1,3 @@
+function nvim --wraps='NVIM_APPNAME="upstream" /opt/nvim-linux-x86_64/bin/nvim' --description 'alias nvim=NVIM_APPNAME="upstream" /opt/nvim-linux-x86_64/bin/nvim'
+    NVIM_APPNAME="upstream" /opt/nvim-linux-x86_64/bin/nvim $argv
+end
